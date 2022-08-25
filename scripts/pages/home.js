7a6d9aea88d9028e7acb30fcb799c29ea580fe46
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-08-25 17:56:27 -0500

    Add home page with logout

--- scripts/pages/home.js
+++ scripts/pages/home.js
@@ -1,9 +1,33 @@
+import { Aside } from "../components/aside.js";
+import { root } from "../config.js";
+import DOMHandler from "../dom-handler.js";
+import { logout } from "../services/sessions.js";
+import LoginPage from "./login-page.js";
+
+function render(){
+  return `
+  <main class="main flex js-home-page">
+  ${Aside()}
+  <h1>Cards</h1>
+  </main>
+  `
+}
+
+function listenLogout(){
+  const signout = document.querySelector(".js-logout");
+  signout.addEventListener("click",()=>{
+    logout().then(DOMHandler.load(LoginPage(),root));
+  })
+}
+
 function HomePage() {
   return {
     toString() {
-      return `<h1>Home</h1>`;
+      return render();
     },
-    addListeners() {},
+    addListeners() {
+      listenLogout()
+    },
   };
 }
 
