1ec8999c8330f0b6a327a5b9c3aee0b1878d6734
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-08-24 21:05:38 -0500

    Add login page

--- scripts/components/input.js
+++ scripts/components/input.js
@@ -1,9 +1,15 @@
 export function Input({ label, placeholder, icon, name, type, alt }) {
   return `
-    <label for="${name}">${label}</label>
-    <div>
-    <img src="${icon} alt="${alt}">
-    <input type="${type}" name="${name} placeholder="${placeholder}>
+  <div class="input-main">
+      <label for="${name}" class="upcase label font-xs">${label}</label>
+      <div class="input-container">
+        <img src="${icon}" alt="${alt}">
+        <input type="${type}"
+          name="${name}"
+          placeholder="${placeholder}"
+          id="${name}"
+          class="text-reg">
+      </div>
     </div>
     `;
 }
--- scripts/pages/login-page.js
+++ scripts/pages/login-page.js
@@ -1,14 +1,43 @@
+import { Input } from "../components/input.js";
 import { Logo } from "../components/logo.js";
 
-function render(){
-    return `
+function render() {
+  return `
     <section class="section">
-      <div class="container">
+      <div class="container login-page">
         ${Logo()}
         <p class="text-center font-xl title">Login</p>
+        <form class="form" id="login-form">
+          ${Input({
+            label: "username",
+            placeholder: "RoLo",
+            icon: "/assets/images/user.svg",
+            name: "username",
+            type: "text",
+            alt: "Username logo",
+          })}
+          ${Input({
+            label: "password",
+            placeholder: "******",
+            icon: "/assets/images/key.svg",
+            name: "password",
+            type: "password",
+            alt: "Password logo",
+          })}
+          <button type="submit" class="button font-s upcase">Login</button>
+          <p class="text-center anchor-red font-reg">Create Account</p>
+        </form>
       </div>
     </section>
-    `
+    `;
+}
+
+function listenSubmit() {
+  const form = document.querySelector("#login-form");
+  form.addEventListener((e) => {
+    e.preventDefault();
+    console.log(e);
+  });
 }
 
 function LoginPage() {
@@ -16,8 +45,10 @@ function LoginPage() {
     toString() {
       return render();
     },
-    addListeners() {},
+    addListeners() {
+      listenSubmit();
+    },
   };
 }
 
-export default LoginPage
+export default LoginPage;
