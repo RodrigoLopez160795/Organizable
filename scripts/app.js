1963c46bde2e7ff3f338b15a060cac5c0b5aaa92
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-08-24 21:55:07 -0500

    Import root selector in app

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
+import { root } from "./config.js";
 import DOMHandler from "./dom-handler.js";
 import LoginPage from "./pages/login-page.js";
-const root = document.querySelector("#root");
 function App() {
   DOMHandler.load(LoginPage(), root);
 }
