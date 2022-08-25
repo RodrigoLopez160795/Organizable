07578537649eddd4b1b95fb3d9d111cd2bd7a424
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-08-24 21:55:31 -0500

    Finish login page without services

--- scripts/pages/login-page.js
+++ scripts/pages/login-page.js
@@ -1,5 +1,14 @@
 import { Input } from "../components/input.js";
 import { Logo } from "../components/logo.js";
+import { root } from "../config.js";
+import DOMHandler from "../dom-handler.js";
+import CreateAccountPage from "./create-account-page.js";
+import HomePage from "./home.js";
+
+let errors = [];
+function renderErrors(error){
+  return `<p class="error">${error}</p>`
+}
 
 function render() {
   return `
@@ -7,7 +16,8 @@ function render() {
       <div class="container login-page">
         ${Logo()}
         <p class="text-center font-xl title">Login</p>
-        <form class="form" id="login-form">
+        <form class="form" id="js-login-form">
+        ${errors.map(renderErrors).join("")}
           ${Input({
             label: "username",
             placeholder: "RoLo",
@@ -25,19 +35,44 @@ function render() {
             alt: "Password logo",
           })}
           <button type="submit" class="button font-s upcase">Login</button>
-          <p class="text-center anchor-red font-reg">Create Account</p>
+          <p class="text-center anchor-red font-reg" id="js-create-account">Create Account</p>
         </form>
       </div>
     </section>
     `;
 }
-
+function listenErrors(...values) {
+  const [ username, password ] = values;
+  console.log(username,password)
+  if (username === "") {
+    errors.push("Need to specify a username");
+  }
+  if (password === "") {
+    errors.push("Need to specify a password");
+  }
+}
 function listenSubmit() {
-  const form = document.querySelector("#login-form");
-  form.addEventListener((e) => {
+  errors = []
+  const form = document.querySelector("#js-login-form");
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(e);
+    const { username, password } = e.target.elements;
+    listenErrors(username.value, password.value);
+    if(errors.length > 0){
+      DOMHandler.reload();
+    }
+    else{
+      DOMHandler.load(HomePage(),root)
+    }
   });
+}
+
+function listenCreate(){
+  const account = document.querySelector("#js-create-account");
+  account.addEventListener("click",()=>{
+    DOMHandler.load(CreateAccountPage(),root)
+  })
+
 }
 
 function LoginPage() {
@@ -47,6 +82,7 @@ function LoginPage() {
     },
     addListeners() {
       listenSubmit();
+      listenCreate();
     },
   };
 }
