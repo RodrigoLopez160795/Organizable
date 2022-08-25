c351b056a5c0ff2dcea26dfbd31dc28f6a43f196
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-08-25 15:24:35 -0500

    Add sign in service

--- scripts/services/sessions.js
+++ scripts/services/sessions.js
@@ -14,8 +14,24 @@ async function login(credentials = { username, password }) {
     }
     sessionStorage.setItem(tokenKey, user.token);
     return user;
-    
-  // if (!response.ok) throw new Error(user.errors.message);
 }
+
+async function signin(credentials = {username,email,first_name,last_name,password}){
+  const response = await fetch(`${BASE_URI}/users`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(credentials),
+  });
+  const user = await response.json();
+  if(!response.ok){
+      return user.errors.message;
+  }
+  sessionStorage.setItem(tokenKey, user.token);
+  return user;
+}
+
+
 
 export { login };
