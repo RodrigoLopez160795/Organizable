e032abe7bf3986688ab8867aa76a2152ff0b2358
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-08-25 17:56:01 -0500

    Add logout service

--- scripts/services/sessions.js
+++ scripts/services/sessions.js
@@ -1,22 +1,24 @@
 import { BASE_URI, tokenKey } from "../config.js";
 
 async function login(credentials = { username, password }) {
-    const response = await fetch(`${BASE_URI}/login`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify(credentials),
-    });
-    const user = await response.json();
-    if(!response.ok){
-        return user.errors.message;
-    }
-    sessionStorage.setItem(tokenKey, user.token);
-    return user;
+  const response = await fetch(`${BASE_URI}/login`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(credentials),
+  });
+  const user = await response.json();
+  if (!response.ok) {
+    return user.errors.message;
+  }
+  sessionStorage.setItem(tokenKey, user.token);
+  return user;
 }
 
-async function signin(credentials = {username,email,first_name,last_name,password}){
+async function signin(
+  credentials = { username, email, first_name, last_name, password }
+) {
   const response = await fetch(`${BASE_URI}/users`, {
     headers: {
       "Content-Type": "application/json",
@@ -25,13 +27,30 @@ async function signin(credentials = {username,email,first_name,last_name,passwor
     body: JSON.stringify(credentials),
   });
   const user = await response.json();
-  if(!response.ok){
-      return user.errors.message;
+  if (!response.ok) {
+    return user.errors.message;
   }
   sessionStorage.setItem(tokenKey, user.token);
   return user;
 }
 
+async function logout() {
+  const token = sessionStorage.getItem(tokenKey);
+  const response = await fetch(`${BASE_URI}/logout`, {
+    headers: {
+      Authorization: `Token token=${token}`,
+    },
+    method: "POST",
+  });
+  let data;
+  try {
+    data = await response.json()
+  } catch (error) {
+    data = response.status.text
+  }
+  if (!response.ok) throw new Error(data.errors);
+  sessionStorage.removeItem(tokenKey, response.token);
+  return data;
+}
 
-
-export { login, signin };
+export { login, signin, logout };
