540ceab61a5f735cff1196b02eb6594fb07a7cbb
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-08-25 19:02:11 -0500

    Add pages to store

--- scripts/store.js
+++ scripts/store.js
@@ -1,8 +1,11 @@
 const STORE = {
   pages: {
-    login: ()=> `Login Page`,
+    login: ()=> `login Page`,
     home: ()=>`home`,
     create_account: ()=>`create_account`,
+    my_boards: ()=> `my_boards`,
+    my_profile: ()=> `my_profile`,
+    closed_boards: ()=> `closed_boards`
     },
     currentPage(){
       return localStorage.getItem("current_page") || this.pages.login()
