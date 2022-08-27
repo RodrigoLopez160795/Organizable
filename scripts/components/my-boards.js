118da07e3d285e0a8447c24b2f5251dd4ea8a41a
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-08-27 18:13:27 -0500

    Add boards with styles

--- scripts/components/my-boards.js
+++ scripts/components/my-boards.js
@@ -1,11 +1,44 @@
+import { getBoards } from "../services/boards.js"
+
+async function renderBoards(){
+  const boards = await getBoards();
+  let result= boards.reduce((boardsArr,board)=>
+  (boardsArr += `
+    <div class="${board.color.split(" ").join("-")} board js-board">
+      <p class="font-boards">${board.name}</p>
+      <div class="board-buttons-container">
+        <button class="board-button"><img src="./assets/images/trashBoard.svg"></button>
+        <button class="board-button"><img src="./assets/images/star.svg"></button>
+      </div>
+    </div>
+  `
+  ),"")
+  return renderBoard(result,boardsListeners())
+}
+
+function renderBoard(boards){
+  const element = document.querySelector(".js-boards");
+  element.innerHTML = boards;
+  boardsListeners()
+}
+
 export function MyBoards(){
+  renderBoards()
     return `
     <section class="section">
-      <div class="container">
+      <div class="container my-boards-container">
         <p class="title font-xl">My boards</p>
         <p class="title font-lg">Boards</p>
+        <div class="boards">
+        <div class="js-boards boards-container"></div>
+        <button class="font-boards create-board-button" id="js-create-board">Create Board</button>
+        </div>
       </div>
     </section>
-    
     `
+}
+
+function boardsListeners(){
+  const boards = document.querySelectorAll(".js-board")
+
 }
